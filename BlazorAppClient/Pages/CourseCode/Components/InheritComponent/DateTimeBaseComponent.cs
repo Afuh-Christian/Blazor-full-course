de92a96a099a1357895be9c3253ef583949d0d8e
Afuh Christian <afuhchristian10@gmail.com>
2023-07-15 21:47:03 +0100

    first commit

---
+++ BlazorAppClient/Pages/CourseCode/Components/InheritComponent/DateTimeBaseComponent.cs
@@ -0,0 +1,11 @@
+﻿
+using Microsoft.AspNetCore.Components;
+using System;
+
+namespace BlazorAppClient.Pages.CourseCode.Components.InheritComponent
+{
+    public class DateTimeBaseComponent : ComponentBase
+    {
+        public DateTime dateTime { get; set; } 
+    }
+}
